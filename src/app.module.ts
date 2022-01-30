dddff1066c4c0602fd6dfc56da47d51fd6df50f6
ajiways <ajiwayswc@gmail.com>
2022-01-31 03:00:51 +0700

    Первичные настройки перед разработкой

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,14 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { typeOrmConfigAsync } from './config/typeorm';
 
-@Module({})
+@Module({
+  imports: [
+    TypeOrmModule.forRootAsync(typeOrmConfigAsync),
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+  ],
+})
 export class AppModule {}
