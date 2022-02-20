d9fd19a8abbb377ba242e09e6e1c4f5facc0addf
sergej.yakovlev <ajiwayswc@gmail.com>
2022-02-20 20:40:00 +0700

    Фикс названий в дто группы

--- src/modules/group/dtos/delete-group.dto.ts
+++ src/modules/group/dtos/delete-group.dto.ts
@@ -2,5 +2,5 @@ import { Length } from 'class-validator';
 
 export class DeleteGroupDto {
   @Length(3, 16, { message: 'Не менее 3 и не более 16 символов' })
-  name: string;
+  groupName: string;
 }
--- src/modules/group/dtos/update-group.dto.ts
+++ src/modules/group/dtos/update-group.dto.ts
@@ -2,5 +2,5 @@ import { Length } from 'class-validator';
 
 export class UpdateGroupDto {
   @Length(3, 16, { message: 'Не менее 3 и не более 16 символов' })
-  name: string;
+  groupName: string;
 }
