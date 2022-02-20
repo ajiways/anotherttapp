eaa90e6871a9d323028de6d34064c4e7dafd2adf
sergej.yakovlev <ajiwayswc@gmail.com>
2022-02-20 20:39:26 +0700

    Изменил ошибку валидации

--- src/exceptions/validation.exception.ts
+++ src/exceptions/validation.exception.ts
@@ -1,10 +1,8 @@
 import { HttpException, HttpStatus } from '@nestjs/common';
+import { IErrArr } from '../interfaces/errors-arr.interface';
 
 export class ValidationException extends HttpException {
-  messages;
-
-  constructor(response) {
+  constructor(response: IErrArr) {
     super(response, HttpStatus.BAD_REQUEST);
-    this.messages = response;
   }
 }
